5900cbd173ae52efc88a62ac9d59a9c0ef267fb7
kan <57279563+waritkan@users.noreply.github.com>
2021-11-21 18:41:35 +0700

    Delete MyDog.java

--- MyDog.java
+++
@@ -1,8 +0,0 @@
-public class MyDog {
-    public static void main(String [] args){
-        Dog doggo=new Dog("white", 15.5f);
-        System.out.println("The color of my dog is "+ doggo.getColor());
-        System.out.println("The weight of my dog is "+ doggo.getWeight());
-    }
-    
-}
