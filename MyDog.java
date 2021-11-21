0a5db9ed4ee466d4007d8cd87239724618c6b113
kan <57279563+waritkan@users.noreply.github.com>
2021-11-21 18:41:04 +0700

    first commit

---
+++ MyDog.java
@@ -0,0 +1,8 @@
+public class MyDog {
+    public static void main(String [] args){
+        Dog doggo=new Dog("white", 15.5f);
+        System.out.println("The color of my dog is "+ doggo.getColor());
+        System.out.println("The weight of my dog is "+ doggo.getWeight());
+    }
+    
+}
